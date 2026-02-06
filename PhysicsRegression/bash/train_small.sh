9f288886e144e5222e465457ec39252b8aab308a
lkyu <1423782024@qq.com>
2026-02-06 15:17:25 +0800

    keep torch's train_small same with paddle's

--- PhysicsRegression/bash/train_small.sh
+++ PhysicsRegression/bash/train_small.sh
@@ -1,23 +1,24 @@
 python ./train.py \
-        --max_epoch 1 \
+        --max_epoch 20 \
         --dump_path ./ \
         --exp_name test \
         --exp_id 0 \
         --n_steps_per_epoch 500 \
-        --collate_queue_size 200 \
-        --batch_size 1 \
+        --optimizer adam_inverse_sqrt,warmup_updates=1000 \
+        --collate_queue_size 500 \
+        --batch_size 16 \
         --save_periodic -1 \
         --save_periodic_from 40\
-        --eval_size 5 \
-        --batch_size_eval 1 \
-        --num_workers 12 \
+        --eval_size 16 \
+        --batch_size_eval 16 \
+        --num_workers 0 \
         --max_len 200 \
         --max_number_bags -1 \
         --max_input_points 200 \
-        --tokens_per_batch 200 \
+        --tokens_per_batch 5000 \
         --add_consts 1 \
         --device "cuda:0" \
-        --use_exprs -1 \
+        --use_exprs 50000 \
         --use_dimension_mask 0 \
         --expr_train_data_path "./data/exprs_train.json" \
         --expr_valid_data_path "./data/exprs_valid.json" \
@@ -26,6 +27,6 @@ python ./train.py \
         --decode_physical_units "single-seq" \
         --use_hints "units,complexity,unarys,consts" \
         --random_variables_sequence 0 \
-        --max_trials 10\
+        --max_trials 5\
         --generate_datapoints_distribution "positive,multi"\
         --rescale 0 \
